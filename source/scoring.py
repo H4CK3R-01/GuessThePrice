7b6a1184b021616eca7e967f1e94368e96140aac
Rripped <linusmaxeickhoff@gmail.com>
2022-05-16 16:01:25 +0200

    added scoring file

---
+++ source/scoring.py
@@ -0,0 +1,11 @@
+"""
+script with functions for evaluating scores of users
+"""
+__author__ = "Florian Kellermann, Linus Eickhoff, Florian Kaiser"
+__date__ = "02.05.2022"
+__version__ = "0.0.1"
+__license__ = "None"
+
+def eval_score(price, guessed_price):
+    return 0
+
